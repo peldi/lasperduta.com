fbf3966a957b5c1a9e2792e6f40caed6653d8cac
Peldi Guilizzoni <peldi@balsamiq.com>
2014-07-11 00:05:58 +0200

    Adding deploy to gh-pages script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Building the site"
+grunt build_all
+
+echo "Copying dist to ~/lasperduta.com.dist"
+cp -r dist ~/lasperduta.com.dist
+
+echo "Switching to gh_pages"
+git checkout gh-pages
+
+rm -rf
+cp -r ~/lasperduta.com.dist/ .
+
+git commit -m "$1"
+git checkout master
